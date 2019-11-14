065acd6ef061b963731386e92e6e17d5a1503b55
Nate Heeren <natedheeren@gmail.com>
2019-11-14 13:17:23 -0800

    first commit for restful tasks ctd

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 
 import { HttpService } from './http.service';
 
@@ -7,7 +7,28 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'public';
+  tasks = [];
+  
   constructor(private _httpService: HttpService){} // Inject the service into the root component
+  
+  ngOnInit(){
+    this.getTasksFromService();
+  }
+
+  getTasksFromService(){
+    let observable = this._httpService.getTasks();
+    observable.subscribe(data => {
+      console.log("Got our tasks!", data);
+
+      for (var key in data)
+      {
+        this.tasks.push(data[key]);
+      }
+
+      // console.log('What follows is this.tasks');
+      // console.log(this.tasks);
+    });
+}
 }
--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -21,9 +21,11 @@ export class HttpService {
   getTasks()
   {
     // our http response is an Observable, store it in a variable
-    let tempObservable = this._http.get('/allTasks');
+    // let tempObservable = this._http.get('/allTasks');
     // subscribe to the Observable and provide the code we would like to do with our data from the response
-    tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    // tempObservable.subscribe(data => console.log("Got our tasks!", data));
+
+    return this._http.get('/allTasks');
   }
 
   // /tasks/:id
